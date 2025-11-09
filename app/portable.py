15d632df422d043b504a0f0d74fb7d110012646c
miatool-marco <marco@miatool.com>
2025-11-09 11:36:30 +0100

    Add more realistic request headers

--- app/portable.py
+++ app/portable.py
@@ -8,7 +8,13 @@ from urllib.parse import urlparse, urljoin
 app = flask.Flask(__name__)
 CORS(app)
 googlebot_headers = {
-    "User-Agent": "Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.6533.119 Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"
+    "User-Agent": "Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.6533.119 Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Accept-Encoding": "gzip, deflate, br",
+    "DNT": "1",
+    "Connection": "keep-alive",
+    "Upgrade-Insecure-Requests": "1"
 }
 html = """
 <!DOCTYPE html>
